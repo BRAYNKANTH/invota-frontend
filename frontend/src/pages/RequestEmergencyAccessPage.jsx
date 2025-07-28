e145ed36667e676e116868792ac34c51976df332
Braynkanth <braynka-im22062@stu.kln.ac.lk>
2025-07-28 13:26:40 +0530

    sdfds

--- frontend/src/pages/RequestEmergencyAccessPage.jsx
+++ frontend/src/pages/RequestEmergencyAccessPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';  // To get the userId from location state
 import axios from 'axios';
 
